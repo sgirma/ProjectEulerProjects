96518e4ef5b0e41e4876149a522c0030dad00216
Sai Grr <sgirma53@gmail.com>
2016-06-17 12:55:57 -0400

    find better solution to find largest prime factors

--- src/projecteulerprojects/LargestPrimeFactors.java
+++ src/projecteulerprojects/LargestPrimeFactors.java
@@ -3,6 +3,7 @@ package projecteulerprojects;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Stack;
 
 /*
  * The prime factors of 13195 are 5, 7, 13 and 29.
@@ -10,15 +11,23 @@ import java.util.List;
  * 
  */
 public class LargestPrimeFactors {
+	private static List<Long> pFactors = new ArrayList<Long>();
+	private static Stack<Long> pFactorsStack = new Stack<Long>();
+    private long product = 1;
+    private long productStack = 1;
 	static HashSet<Long> primeFactors = new HashSet<Long>();
 	
 	public static void main(String[] args) {
 		long n = 600851475143L;
-//		n = 13195;
+		n = 131857;
+		int product = 1;
 		HashSet<Long> arnd = findLargestPrimeFactors(n);
 		for(Long l : arnd) {
 			System.out.println(l);
+			product *= l;
 		}
+		
+		System.out.println(product);
 	}
 	
 	public static HashSet<Long> findLargestPrimeFactors(long n) {
@@ -38,6 +47,66 @@ public class LargestPrimeFactors {
 		}
 		
 		return primeFactors;
+	}
+	
+	/**
+	 * improve findLargestPrimeFactors
+	 * @param n
+	 * @return
+	 */
+	public List<Long> largestPrimeFactors(long n) {       
+		if(isPrime(n)) {
+			product *= n;
+			pFactors.add(n);
+			return pFactors;
+		}
+
+		for(int i = 0; i < n; i++) {
+			if(product >= n) {
+				return pFactors;
+			}
+			try {
+				if(isPrime(i) && n % i == 0) {
+					product *= i;
+					pFactors.add((long)i);
+					largestPrimeFactors(n/i);
+				}
+			} catch(ArithmeticException ae) {
+				continue;
+			}
+		}
+
+		return pFactors;
+	}
+	
+	/**
+	 * improve findLargestPrimeFactors
+	 * @param n
+	 * @return
+	 */
+	public Stack<Long> largestPrimeFactorsStack(long n) {       
+		if(isPrime(n)) {
+			productStack *= n;
+			pFactorsStack.push(n);
+			return pFactorsStack;
+		}
+
+		for(int i = 0; i < n; i++) {
+			if(productStack >= n) {
+				return pFactorsStack;
+			}
+			try {
+				if(isPrime(i) && n % i == 0) {
+					productStack *= i;
+					pFactorsStack.push((long)i);
+					largestPrimeFactorsStack(n/i);
+				}
+			} catch(ArithmeticException ae) {
+				continue;
+			}
+		}
+
+		return pFactorsStack;
 	}
 	
 	private static boolean isPrime(long value) {
---
+++ src/test/LargestPrimeFactorsTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import projecteulerprojects.LargestPrimeFactors;
+
+public class LargestPrimeFactorsTest {
+	@Test
+	public void testLargestPrimeFactorsStack() {
+		LargestPrimeFactors lpf = new LargestPrimeFactors();
+		long n = 600851475143L;
+		
+		assertEquals((Long) 6857L, lpf.largestPrimeFactorsStack(n).peek());
+	}
+
+
+	@Test
+	public void testLargestPrimeFactors() {
+		LargestPrimeFactors lpf = new LargestPrimeFactors();
+		long n = 600851475143L;
+		List<Long> pFactors = new ArrayList<Long>();
+		pFactors.add(71L);
+		pFactors.add(839L);
+		pFactors.add(1471L);
+		pFactors.add(6857L);
+		
+		assertEquals(pFactors, lpf.largestPrimeFactors(n));
+	}
+}
