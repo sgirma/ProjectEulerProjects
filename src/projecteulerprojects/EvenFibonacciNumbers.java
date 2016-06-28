d060a0bc103510e50b96a043aa085ad9333ad6f7
S <sgirma53@gmail.com>
2016-06-27 21:26:23 -0400

    Update EvenFibonacciNumbers.java

--- src/projecteulerprojects/EvenFibonacciNumbers.java
+++ src/projecteulerprojects/EvenFibonacciNumbers.java
@@ -11,7 +11,7 @@ import base.BaseMethods;
  * 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
  * By considering the terms in the Fibonacci sequence whose values do not exceed four million,
  * find the sum of the even-valued terms.
- * @author saigrr
+ * @author sgirma
  *
  */
 public class EvenFibonacciNumbers extends BaseMethods {
